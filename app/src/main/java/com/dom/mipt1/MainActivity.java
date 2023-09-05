5f99e842f85597eb943438346a7e209094cf9179
DOM <domantasjanus@gmail.com>
2023-09-05 17:47:14 +0300

    Added text box and button in layout.xml. Clicked button will change text box content.

--- app/src/main/java/com/dom/mipt1/MainActivity.java
+++ app/src/main/java/com/dom/mipt1/MainActivity.java
@@ -1,13 +1,28 @@
 package com.dom.mipt1;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout);
+        initComponents();
+    }
+
+    private void initComponents() {
+        //First button changes TextBox content.
+        Button textChangeBtn = findViewById(R.id.btn1);
+        textChangeBtn.setOnClickListener((event) -> onBtn1Click());
+    }
+
+
+    public void onBtn1Click() {
+        TextView textBox = findViewById(R.id.textBox);
+        textBox.setText("Hello VU KNF!");
     }
 }
