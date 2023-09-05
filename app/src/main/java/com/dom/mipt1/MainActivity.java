a846b5a02d0197827ac0057e101cf745e95cd7a0
DOM <domantasjanus@gmail.com>
2023-09-05 18:00:33 +0300

    A second button has been added to change the text colour.

--- app/src/main/java/com/dom/mipt1/MainActivity.java
+++ app/src/main/java/com/dom/mipt1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dom.mipt1;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,11 +19,24 @@ public class MainActivity extends AppCompatActivity {
         //First button changes TextBox content.
         Button textChangeBtn = findViewById(R.id.btn1);
         textChangeBtn.setOnClickListener((event) -> onBtn1Click());
+
+        //Second button changes TextBox color to green. Otherwise, text color will be set to Black.
+        Button colorChangeBtn = findViewById(R.id.btn2);
+        colorChangeBtn.setOnClickListener((event) -> onBtn2Click());
     }
 
 
     public void onBtn1Click() {
         TextView textBox = findViewById(R.id.textBox);
         textBox.setText("Hello VU KNF!");
+    }
+
+    public void onBtn2Click() {
+        TextView textBox = findViewById(R.id.textBox);
+        if (textBox.getCurrentTextColor() != Color.GREEN) {
+            textBox.setTextColor(Color.GREEN);
+            return;
+        }
+        textBox.setTextColor(Color.BLACK);
     }
 }
